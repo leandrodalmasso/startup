403df33f8a5bd1a29c66fab794d612afb3401d0a
leandrodalmasso <leandrodalmasso@gmail.com>
2018-05-18 15:19:58 -0300

    Exercise 2 corrected

--- 01-JS-DOM-APIs/exercise2/js/main.js
+++ 01-JS-DOM-APIs/exercise2/js/main.js
@@ -4,10 +4,18 @@ function fadein(section) {
   }
 }
 
+function fadeinSection(section) {
+  fadein(section);
+}
+
 let helloWorldSection = document.getElementById("hello-world");
 
-document.addEventListener("load", fadein(helloWorldSection));
+document.addEventListener("load", fadeinSection(helloWorldSection));
+
+function showAlert() {
+  alert("Great! Now press OK.");
+}
 
 let alertButton = document.getElementById("alert-button");
 
-alertButton.addEventListener("click", () => alert("Great! Now press OK."));
+alertButton.addEventListener("click", showAlert);

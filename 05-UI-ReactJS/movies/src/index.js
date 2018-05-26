be4c04db5acab05c5162774347a2b6dc9bd4ff8d
leandrodalmasso <leandrodalmasso@gmail.com>
2018-05-26 16:31:28 -0300

    Exercise 1

---
+++ 05-UI-ReactJS/movies/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import NewMovieForm from './NewMovieForm';
+
+ReactDOM.render(
+  <NewMovieForm />,
+  document.getElementById('root')
+);
